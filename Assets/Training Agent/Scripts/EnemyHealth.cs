3d31f643748874e2e78ca79604e0c03072acbaca
Alvin Tristandy <alvin.tristandy@binus.ac.id>
2025-04-16 15:43:03 +0900

    Update EnemyHealth.cs

--- Assets/Training Agent/Scripts/EnemyHealth.cs
+++ Assets/Training Agent/Scripts/EnemyHealth.cs
@@ -8,8 +8,17 @@ public class EnemyHealth : MonoBehaviour
 
     public float HealthPoint {get;}
 
+    public void SetHealthPoint(float value){
+        healthPoint = value;
+    }
+
+    public void AddHealthPoint(float value){
+        healthPoint = value;
+    }
+
     public void TakeDamage(float damagePoint){
         healthPoint -= damagePoint;
+        CheckHP();
     }
 
     private void CheckHP(){
